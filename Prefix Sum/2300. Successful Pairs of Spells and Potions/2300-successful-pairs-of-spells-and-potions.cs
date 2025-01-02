d284a9d3df7537f011011bc08d619c648d47916d
Май Ван Туан <8268826@gmail.com>
2025-01-03 00:54:58 +0300

    Time: 16 ms (100%), Space: 87.62 MB (16.54%)

---
+++ Prefix Sum/2300. Successful Pairs of Spells and Potions/2300-successful-pairs-of-spells-and-potions.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] SuccessfulPairs(int[] spells, int[] potions, long success) {
+        var limitPotions = 100001;
+        var prefix = new int[limitPotions]; //prefix number of potions needed to reach success
+
+        foreach(int potion in potions){ //find number of potions needed to reach success
+            var neededSpellCost = (int)((success + potion - 1) / potion);
+            if(neededSpellCost < limitPotions && neededSpellCost >= 0) prefix[neededSpellCost]++;
+        } 
+
+        for(int i = 1; i < limitPotions; i++){ //prefix sum
+            prefix[i] += prefix[i - 1];
+        }
+
+        var pairs = new int[spells.Length];
+        for(int i = 0; i < spells.Length; i++){ //find number of successful pairs
+            pairs[i] = prefix[spells[i]];
+        }
+
+        return pairs;
+    }
+}
