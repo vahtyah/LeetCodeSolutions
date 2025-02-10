c8484ce8b8b39894dd67ae7885ec9c2e43f72ce3
Май Ван Туан <8268826@gmail.com>
2025-02-10 09:58:41 +0300

    Time: 0 ms (100,00%), Space: 41.8 MB MB (83,18%)

---
+++ Data Structures/Stack/3174. Clear Digits/3174-clear-digits.cs
@@ -0,0 +1,21 @@
+namespace LeetCodeSolutions.DataStructures/Stack;
+
+/*
+ * 3174. Clear Digits
+ * Difficulty: Easy
+ * Submission Time: 2025-02-10 06:48:37
+ * Created by vahtyah on 2025-02-10 06:54:39
+*/
+ 
+public class Solution {
+    public string ClearDigits(string s) {
+        var stack = new Stack<char>();
+        
+        for(int i = 0; i < s.Length; i++){
+            if(Char.IsDigit(s[i])) stack.Pop();
+            else stack.Push(s[i]);
+        }
+
+        return string.Concat(stack.Reverse());
+    }
+}
