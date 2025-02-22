da640b21b761ee42d172c7e577da7c2d6b0a9689
Май Ван Туан <8268826@gmail.com>
2025-02-22 13:52:04 +0300

    Time: 0 ms (100,00%), Space: 43.2 MB (54,99%)

---
+++ Recursion/0021. Merge Two Sorted Lists/0021-merge-two-sorted-lists.cs
@@ -0,0 +1,34 @@
+namespace LeetCodeSolutions.Recursion;
+
+/*
+ * 0021. Merge Two Sorted Lists
+ * Difficulty: Easy
+ * Submission Time: 2025-02-22 10:51:28
+ * Created by vahtyah on 2025-02-22 10:52:00
+*/
+ 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;  
+        if (list2 == null) return list1;  
+        
+        if (list1.val <= list2.val) {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
