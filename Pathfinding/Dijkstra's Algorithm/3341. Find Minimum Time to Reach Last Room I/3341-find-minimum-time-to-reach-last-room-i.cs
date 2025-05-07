5dc91a78abbfc3d49baf9ab0d2e6717f43c38a19
Май Ван Туан <8268826@gmail.com>
2025-05-07 10:08:27 +0300

    Time: 111 ms (91,67%), Space: 48.8 MB (58,33%)

---
+++ Pathfinding/Dijkstra's Algorithm/3341. Find Minimum Time to Reach Last Room I/3341-find-minimum-time-to-reach-last-room-i.cs
@@ -0,0 +1,60 @@
+namespace LeetCodeSolutions.Pathfinding/Dijkstra'sAlgorithm;
+
+/*
+ * 3341. Find Minimum Time to Reach Last Room I
+ * Difficulty: Medium
+ * Submission Time: 2025-05-07 06:56:22
+ * Created by vahtyah on 2025-05-07 06:57:18
+*/
+ 
+public class Solution {
+    private static int[][] directions = new int[][] 
+    {
+        new int[] {0, 1},
+        new int[] {1, 0},
+        new int[] {0, -1},
+        new int[] {-1, 0}
+    };
+
+    public int MinTimeToReach(int[][] moveTime) {
+        var m = moveTime.Length;
+        var n = moveTime[0].Length;
+        moveTime[0][0] = 0;
+
+        var distance = new int[m, n];
+        var visited = new bool[m, n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++) distance[i, j] = int.MaxValue;
+        }
+
+        distance[0, 0] = 0;
+
+        var pq = new PriorityQueue<(int row, int col), int>(m * n);
+        pq.Enqueue((0, 0), 0);
+
+        while(pq.Count > 0){
+            var (row, col) = pq.Dequeue();
+
+            if(visited[row, col]) continue;
+            visited[row, col] = true;
+
+            if(row == m - 1 && col == n - 1) return distance[row, col];
+
+            foreach(var dir in directions){
+                var newRow = row + dir[0];
+                var newCol = col + dir[1];
+
+                if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited[newRow, newCol]) continue;
+
+                var newDist = distance[row, col] + 1 + Math.Max(0, moveTime[newRow][newCol] - distance[row, col]);
+                if(newDist < distance[newRow, newCol]){
+                    distance[newRow, newCol] = newDist;
+                    pq.Enqueue((newRow, newCol), newDist);
+                }
+            }
+        }
+
+        return -1;
+    }
+}
