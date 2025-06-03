336024ca419bb5c260e50860392cd5147d208763
Май Ван Туан <8268826@gmail.com>
2025-06-03 09:15:08 +0300

    Time: 5 ms (100,00%), Space: 64.6 MB (83,33%)

--- Searching/Breadth-First Search/1298. Maximum Candies You Can Get from Boxes/1298-maximum-candies-you-can-get-from-boxes.cs
+++ Searching/Breadth-First Search/1298. Maximum Candies You Can Get from Boxes/1298-maximum-candies-you-can-get-from-boxes.cs
@@ -3,25 +3,22 @@ namespace LeetCodeSolutions.Searching/Breadth-FirstSearch;
 /*
  * 1298. Maximum Candies You Can Get from Boxes
  * Difficulty: Hard
- * Submission Time: 2025-06-03 06:04:55
- * Created by vahtyah on 2025-06-03 06:05:47
+ * Submission Time: 2025-06-03 06:14:38
+ * Created by vahtyah on 2025-06-03 06:15:06
 */
  
 public class Solution {
     public int MaxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
         if (initialBoxes.Length == 0) return 0;
         
-        int totalCandies = 0;
-        int n = status.Length;
+        var totalCandies = 0;
         
-        var hasBox = new bool[n];
-        var hasKey = new bool[n];
-        var processed = new bool[n];
+        var boxState = new int[status.Length]; // 0: unknown, 1: have box, 2: have key, 3: have both, 4: processed
         var queue = new Queue<int>();
         
         foreach (int box in initialBoxes) {
-            hasBox[box] = true;
-            if (status[box] == 1) {
+            boxState[box] |= 1; 
+            if (status[box] == 1 || (boxState[box] & 2) != 0) {
                 queue.Enqueue(box);
             }
         }
@@ -29,27 +26,27 @@ public class Solution {
         while (queue.Count > 0) {
             int currentBox = queue.Dequeue();
             
-            if (processed[currentBox]) continue;
-            processed[currentBox] = true;
+            if ((boxState[currentBox] & 4) != 0) continue; 
+            boxState[currentBox] |= 4;
             
             totalCandies += candies[currentBox];
             
             foreach (int key in keys[currentBox]) {
-                if (!hasKey[key]) {
-                    hasKey[key] = true;
-                    if (hasBox[key] && !processed[key]) {
-                        queue.Enqueue(key);
-                    }
+                bool hadKey = (boxState[key] & 2) != 0;
+                boxState[key] |= 2; 
+                
+                if (!hadKey && (boxState[key] & 1) != 0 && (boxState[key] & 4) == 0) {
+                    queue.Enqueue(key);
                 }
             }
             
             foreach (int newBox in containedBoxes[currentBox]) {
-                if (!hasBox[newBox]) {
-                    hasBox[newBox] = true;
-                    if (status[newBox] == 1 || hasKey[newBox]) {
-                        if (!processed[newBox]) {
-                            queue.Enqueue(newBox);
-                        }
+                bool hadBox = (boxState[newBox] & 1) != 0;
+                boxState[newBox] |= 1; 
+                
+                if (!hadBox && (boxState[newBox] & 4) == 0) {
+                    if (status[newBox] == 1 || (boxState[newBox] & 2) != 0) {
+                        queue.Enqueue(newBox);
                     }
                 }
             }
