af218dad2f22c3109e8d2d6b6bf8a688e86e7eb6
Май Ван Туан <8268826@gmail.com>
2025-06-06 10:47:28 +0300

    Time: 0 ms (100,00%), Space: 46.1 MB (85,29%)

--- Bit Manipulation/3151. Special Array I/3151-special-array-i.cs
+++ Bit Manipulation/3151. Special Array I/3151-special-array-i.cs
@@ -4,8 +4,8 @@ namespace LeetCodeSolutions.BitManipulation;
  * 3151. Special Array I
  * Difficulty: Easy
  * Submission Time: 2025-02-01 11:58:53
- * Created by vahtyah on 2025-02-01 12:01:33
- */
+ * Created by vahtyah on 2025-06-06 07:47:25
+*/
  
 public class Solution {
     public bool IsArraySpecial(int[] nums) {
