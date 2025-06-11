e331bb175d6957ba7f40ae135e472e9239ed45d6
Май Ван Туан <8268826@gmail.com>
2025-06-11 09:48:01 +0300

    Time: 1 ms (100,00%), Space: 60.9 MB (98,26%)

--- Dynamic Programming/0053. Maximum Subarray/0053-maximum-subarray.cs
+++ Dynamic Programming/0053. Maximum Subarray/0053-maximum-subarray.cs
@@ -3,18 +3,17 @@ namespace LeetCodeSolutions.DynamicProgramming;
 /*
  * 0053. Maximum Subarray
  * Difficulty: Medium
- * Submission Time: 2025-06-11 06:31:03
- * Created by vahtyah on 2025-06-11 06:31:36
+ * Submission Time: 2025-06-11 06:46:30
+ * Created by vahtyah on 2025-06-11 06:47:10
 */
  
 public class Solution {
     public int MaxSubArray(int[] nums) {
         var maxSoFar = nums[0];
-        var maxEndingHere = nums[0];
+        var maxEndingHere = 0;
 
-        for (int i = 1; i < nums.Length; i++)
-        {
-            maxEndingHere = Math.Max(nums[i], maxEndingHere + nums[i]);
+        foreach(var num in nums){
+            maxEndingHere = Math.Max(num, maxEndingHere + num);
             maxSoFar = Math.Max(maxSoFar, maxEndingHere);
         }
 
