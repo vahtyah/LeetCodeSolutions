1d0db849203ad0b2c399d484f5c31229854500aa
vahtyah <8268826@gmail.com>
2025-08-04 13:33:59 +0700

    Time: 3 ms (100,00%), Space: 53.7 MB (74,26%)

--- Sliding Window/0904. Fruit Into Baskets/0904-fruit-into-baskets.cs
+++ Sliding Window/0904. Fruit Into Baskets/0904-fruit-into-baskets.cs
@@ -3,10 +3,51 @@ namespace LeetCodeSolutions.SlidingWindow;
 /*
  * 0904. Fruit Into Baskets
  * Difficulty: Medium
- * Submission Time: 2025-08-04 06:20:52
- * Created by vahtyah on 2025-08-04 06:22:47
+ * Submission Time: 2025-08-04 06:32:46
+ * Created by vahtyah on 2025-08-04 06:33:50
 */
  
+public class Solution {
+    public int TotalFruit(int[] fruits) {
+        int currentFruit = fruits[0];
+        int previousFruit = -1;
+        
+        int consecutiveCurrentCount = 1;
+        int totalValidWindowLength = 1;
+        
+        int maxValidWindowLength = 1;
+        
+        for (int i = 1; i < fruits.Length; i++)
+        {   
+            if (fruits[i] == currentFruit)
+            {
+                consecutiveCurrentCount++;
+                totalValidWindowLength++;
+            }
+            else if (previousFruit == -1 || fruits[i] == previousFruit)
+            {
+                previousFruit = currentFruit;
+                currentFruit = fruits[i];
+                consecutiveCurrentCount = 1;
+                totalValidWindowLength++;
+            }
+            else 
+            {
+                maxValidWindowLength = Math.Max(maxValidWindowLength, totalValidWindowLength);
+                previousFruit = currentFruit;
+                currentFruit = fruits[i];
+                totalValidWindowLength = consecutiveCurrentCount + 1;
+                consecutiveCurrentCount = 1;
+            }
+        }
+        
+        maxValidWindowLength = Math.Max(maxValidWindowLength, totalValidWindowLength);
+        
+        return maxValidWindowLength;
+    }
+}
+
+
 public class Solution {
     public int TotalFruit(int[] fruits) {
         var n = fruits.Length;
