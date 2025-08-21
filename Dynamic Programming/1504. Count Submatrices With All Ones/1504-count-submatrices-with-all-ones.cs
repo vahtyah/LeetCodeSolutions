5e1169803d6f585be0f1b864fc05e38b82a9287a
vahtyah <8268826@gmail.com>
2025-08-21 19:24:50 +0700

    Time: 7 ms (100,00%), Space: 49.9 MB (22,73%)

--- Dynamic Programming/1504. Count Submatrices With All Ones/1504-count-submatrices-with-all-ones.cs
+++ Dynamic Programming/1504. Count Submatrices With All Ones/1504-count-submatrices-with-all-ones.cs
@@ -3,32 +3,27 @@ namespace LeetCodeSolutions.DynamicProgramming;
 /*
  * 1504. Count Submatrices With All Ones
  * Difficulty: Medium
- * Submission Time: 2025-08-21 12:17:56
- * Created by vahtyah on 2025-08-21 12:18:43
+ * Submission Time: 2025-08-21 12:24:26
+ * Created by vahtyah on 2025-08-21 12:24:49
 */
  
 public class Solution {
     public int NumSubmat(int[][] mat) {
-        int m = mat.Length;
-        int n = mat[0].Length;
+        var m = mat.Length;
+        var n = mat[0].Length;
 
-        // dp[i][j]: number of continuous 1s ending at (i,j) in the current row
-        int[][] dp = new int[m][];
+        var dp = new int[m][];
+
+        var total = 0;
+
         for (int i = 0; i < m; i++) {
             dp[i] = new int[n];
-        }
-
-        int total = 0;
-
-        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 1) {
-                    // count how many continuous 1s in row ending at (i,j)
                     dp[i][j] = (j == 0 ? 0 : dp[i][j - 1]) + 1;
 
-                    int minWidth = dp[i][j];
+                    var minWidth = dp[i][j];
 
-                    // go upwards to count submatrices ending at (i,j)
                     for (int k = i; k >= 0 && dp[k][j] > 0; k--) {
                         minWidth = Math.Min(minWidth, dp[k][j]);
                         total += minWidth;
